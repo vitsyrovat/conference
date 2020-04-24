3915de06ecbe323e971ccee2183aa37393ef9b2a
vitek <vitsyrovat@gmail.com>
2020-04-24 11:56:42 +0200

    Configured postgres db

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -15,7 +15,7 @@ class CommandTest(TestCase):
             call_command('wait_for_db')
             self.assertEqual(gi.call_count, 1)
 
-    @patch('time.sleep', return_value=True) # decorator mocking the behaviour of time.sleep
+    @patch('time.sleep', return_value=True)
     def test_wailt_for_db(self, ts):
         """Test waiting for db"""
         with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
